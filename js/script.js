6dba2f1859348b597eee8c13e2deee457e54eabe
Isaac Vono <isaacvono@outlook.com>
2023-11-20 10:02:33 -0300

    js-perguntas-frequentes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // Ativar Links do Menu
 
-const links = document.querySelectorAll('.header-menu a')
+const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
   if (url.includes(href)) {
-    link.classList.add("ativo")
+    link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
 // Ativar itens do Orçamento
 
@@ -23,4 +23,24 @@ function ativarProduto(parametro) {
   }
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
